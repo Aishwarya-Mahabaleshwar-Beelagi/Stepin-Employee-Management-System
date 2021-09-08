1f734d5dd9f9c39b7bfd824d58c9d4237eb67916
Aishwarya-Mahabaleshwar-Beelagi <89735341+Aishwarya-Mahabaleshwar-Beelagi@users.noreply.github.com>
2021-09-08 20:42:56 +0530

    Add files via upload

---
+++ 3.Implementation/inc/emp.h
@@ -0,0 +1,11 @@
+#ifndef __RECORD_H__
+#define __RECORD_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void Add_record();
+void Modify_record();
+void Delete_record();
+void Display_record();
+#endif 
